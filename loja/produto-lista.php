628e4e690555fa8ef4e0f39f6de5f2d193637b8f
unknown <fabioviniciuslds@hotmail.com>
2017-01-11 14:49:27 -0200

    corrigindo alterando produto

--- loja/produto-lista.php
+++ loja/produto-lista.php
@@ -27,7 +27,7 @@ include("banco-categoria.php");
 			<td><?= $produto['preco']?></td>
 			<td><?= substr($produto['descricao'], 0,40)?></td>
 			<td><?= $produto['categoria_nome']?></td>
-			 <td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id']?>">alterar</a>
+			 <td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id']?>">alterar</a><td>
 
 			<td>
 				<form action="remove-produto.php" method="post">
